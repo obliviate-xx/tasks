d2103b89a9942f12ffc0855be6ef276f86662874
codesktop <codesktop@foxmail.com>
2021-07-23 16:21:40 +0800

    init commit

--- youtube-daily.js
+++ youtube-daily.js
@@ -2,10 +2,10 @@ const youtube = require('obliviate/lib/tasks/youtube')
 const store = require('obliviate/lib/store')
 
 async function youtubeDaily() {
-  const { data: { official, fancam } } = await store.get('/b/6002f92ef98f6e35d5fd3b51/latest')
+  const { data: { official, fancam } } = await store.get('/b/lNi2GNTny6')
 
-  await youtube.perform({ id: official.join(','), binId: '5ffea24768f9f835a3de98d6' })
-  await youtube.perform({ id: fancam.join(','), binId: '5ffeb153f98f6e35d5fbf31f' })
+  await youtube.perform({ id: official.join(','), binId: 'cQ1upQRufO' })
+  await youtube.perform({ id: fancam.join(','), binId: '3F74qXVfQR' })
 }
 
 youtubeDaily()
